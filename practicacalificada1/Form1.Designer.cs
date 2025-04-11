994252fb9f88c96adc1c542ba0edb9fac30f59e7
Usuario <Usuario@EPIS>
2025-04-11 12:20:35 -0500

    Agregar archivos de proyecto.

---
+++ practicacalificada1/Form1.Designer.cs
@@ -0,0 +1,175 @@
+﻿namespace practicacalificada1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.spAgregarT = new System.Windows.Forms.Button();
+            this.spEliminarT = new System.Windows.Forms.Button();
+            this.spListarT = new System.Windows.Forms.Button();
+            this.spActualizarT = new System.Windows.Forms.Button();
+            this.txtCodAlumno = new System.Windows.Forms.TextBox();
+            this.txtAPaterno = new System.Windows.Forms.TextBox();
+            this.txtAMaterno = new System.Windows.Forms.TextBox();
+            this.txtNombres = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtCodCarrera = new System.Windows.Forms.TextBox();
+            this.dgvTAlumnos = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTAlumnos)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // spAgregarT
+            // 
+            this.spAgregarT.Location = new System.Drawing.Point(708, 39);
+            this.spAgregarT.Name = "spAgregarT";
+            this.spAgregarT.Size = new System.Drawing.Size(128, 58);
+            this.spAgregarT.TabIndex = 0;
+            this.spAgregarT.Text = "Agregar";
+            this.spAgregarT.UseVisualStyleBackColor = true;
+            this.spAgregarT.Click += new System.EventHandler(this.spAgregarT_Click);
+            // 
+            // spEliminarT
+            // 
+            this.spEliminarT.Location = new System.Drawing.Point(897, 39);
+            this.spEliminarT.Name = "spEliminarT";
+            this.spEliminarT.Size = new System.Drawing.Size(128, 58);
+            this.spEliminarT.TabIndex = 1;
+            this.spEliminarT.Text = "Eliminar";
+            this.spEliminarT.UseVisualStyleBackColor = true;
+            this.spEliminarT.Click += new System.EventHandler(this.spEliminarT_Click);
+            // 
+            // spListarT
+            // 
+            this.spListarT.Location = new System.Drawing.Point(708, 115);
+            this.spListarT.Name = "spListarT";
+            this.spListarT.Size = new System.Drawing.Size(128, 58);
+            this.spListarT.TabIndex = 2;
+            this.spListarT.Text = "Listar";
+            this.spListarT.UseVisualStyleBackColor = true;
+            this.spListarT.Click += new System.EventHandler(this.spListarT_Click);
+            // 
+            // spActualizarT
+            // 
+            this.spActualizarT.Location = new System.Drawing.Point(897, 115);
+            this.spActualizarT.Name = "spActualizarT";
+            this.spActualizarT.Size = new System.Drawing.Size(128, 58);
+            this.spActualizarT.TabIndex = 3;
+            this.spActualizarT.Text = "Actualizar";
+            this.spActualizarT.UseVisualStyleBackColor = true;
+            this.spActualizarT.Click += new System.EventHandler(this.spActualizarT_Click);
+            // 
+            // txtCodAlumno
+            // 
+            this.txtCodAlumno.Location = new System.Drawing.Point(330, 24);
+            this.txtCodAlumno.Name = "txtCodAlumno";
+            this.txtCodAlumno.Size = new System.Drawing.Size(230, 20);
+            this.txtCodAlumno.TabIndex = 4;
+            // 
+            // txtAPaterno
+            // 
+            this.txtAPaterno.Location = new System.Drawing.Point(330, 67);
+            this.txtAPaterno.Name = "txtAPaterno";
+            this.txtAPaterno.Size = new System.Drawing.Size(230, 20);
+            this.txtAPaterno.TabIndex = 5;
+            // 
+            // txtAMaterno
+            // 
+            this.txtAMaterno.Location = new System.Drawing.Point(330, 113);
+            this.txtAMaterno.Name = "txtAMaterno";
+            this.txtAMaterno.Size = new System.Drawing.Size(230, 20);
+            this.txtAMaterno.TabIndex = 6;
+            // 
+            // txtNombres
+            // 
+            this.txtNombres.Location = new System.Drawing.Point(330, 161);
+            this.txtNombres.Name = "txtNombres";
+            this.txtNombres.Size = new System.Drawing.Size(230, 20);
+            this.txtNombres.TabIndex = 7;
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(330, 216);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(230, 20);
+            this.txtUsuario.TabIndex = 8;
+            // 
+            // txtCodCarrera
+            // 
+            this.txtCodCarrera.Location = new System.Drawing.Point(330, 266);
+            this.txtCodCarrera.Name = "txtCodCarrera";
+            this.txtCodCarrera.Size = new System.Drawing.Size(230, 20);
+            this.txtCodCarrera.TabIndex = 9;
+            // 
+            // dgvTAlumnos
+            // 
+            this.dgvTAlumnos.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvTAlumnos.Location = new System.Drawing.Point(96, 312);
+            this.dgvTAlumnos.Name = "dgvTAlumnos";
+            this.dgvTAlumnos.Size = new System.Drawing.Size(959, 230);
+            this.dgvTAlumnos.TabIndex = 10;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1117, 562);
+            this.Controls.Add(this.dgvTAlumnos);
+            this.Controls.Add(this.txtCodCarrera);
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtNombres);
+            this.Controls.Add(this.txtAMaterno);
+            this.Controls.Add(this.txtAPaterno);
+            this.Controls.Add(this.txtCodAlumno);
+            this.Controls.Add(this.spActualizarT);
+            this.Controls.Add(this.spListarT);
+            this.Controls.Add(this.spEliminarT);
+            this.Controls.Add(this.spAgregarT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvTAlumnos)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button spAgregarT;
+        private System.Windows.Forms.Button spEliminarT;
+        private System.Windows.Forms.Button spListarT;
+        private System.Windows.Forms.Button spActualizarT;
+        private System.Windows.Forms.TextBox txtCodAlumno;
+        private System.Windows.Forms.TextBox txtAPaterno;
+        private System.Windows.Forms.TextBox txtAMaterno;
+        private System.Windows.Forms.TextBox txtNombres;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.TextBox txtCodCarrera;
+        private System.Windows.Forms.DataGridView dgvTAlumnos;
+    }
+}
+
---
+++ practicacalificada1/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Data.SqlClient;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace practicacalificada1
+{
+    public partial class Form1 : Form
+    {
+        
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        static private string cadena = "server=dataepis.uandina.pe,49157;Database=BDAcademicoGamarra;Uid=gamarrae;pwd=vacaconcaca123";
+        static private SqlConnection conexion = new SqlConnection(cadena);
+
+        private void Listar()
+        {
+            SqlCommand comando = new SqlCommand("spListarT", conexion);
+            comando.CommandType = CommandType.StoredProcedure;
+            SqlDataAdapter adapter = new SqlDataAdapter(comando);
+            DataTable tabla = new DataTable();
+            adapter.Fill(tabla);
+            dgvTAlumnos.DataSource = tabla;
+        }
+
+        private void spEliminarT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand comando = new SqlCommand("spEliminarT", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@CodAlumno", txtCodAlumno.Text);
+
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+
+                MessageBox.Show("Alumno eliminado correctamente.");
+                Listar(); // Refrescar tabla
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al eliminar: " + ex.Message);
+                conexion.Close();
+            }
+        }
+
+        private void spListarT_Click(object sender, EventArgs e)
+        {
+          
+        }
+
+        private void spActualizarT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand comando = new SqlCommand("spActualizarT", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@CodAlumno", txtCodAlumno.Text);
+                comando.Parameters.AddWithValue("@APaterno", txtAPaterno.Text);
+                comando.Parameters.AddWithValue("@AMaterno", txtAMaterno.Text);
+                comando.Parameters.AddWithValue("@Nombres", txtNombres.Text);
+                comando.Parameters.AddWithValue("@Usuario", txtUsuario.Text);
+                comando.Parameters.AddWithValue("@CodCarrera", txtCodCarrera.Text);
+
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+
+                MessageBox.Show("Alumno actualizado correctamente.");
+                Listar(); // Refrescar la tabla
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al actualizar: " + ex.Message);
+                conexion.Close();
+            }
+        }
+
+        private void spAgregarT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                SqlCommand comando = new SqlCommand("spAgregarT", conexion);
+                comando.CommandType = CommandType.StoredProcedure;
+
+                comando.Parameters.AddWithValue("@CodAlumno", txtCodAlumno.Text);
+                comando.Parameters.AddWithValue("@APaterno", txtAPaterno.Text);
+                comando.Parameters.AddWithValue("@AMaterno", txtAMaterno.Text);
+                comando.Parameters.AddWithValue("@Nombres", txtNombres.Text);
+                comando.Parameters.AddWithValue("@Usuario", txtUsuario.Text);
+                comando.Parameters.AddWithValue("@CodCarrera", txtCodCarrera.Text);
+
+                conexion.Open();
+                comando.ExecuteNonQuery();
+                conexion.Close();
+
+                MessageBox.Show("Alumno agregado correctamente.");
+                Listar(); // Actualizar la tabla en pantalla
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error al agregar: " + ex.Message);
+                conexion.Close();
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            Listar();
+        }
+
+        private void textBox7_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
